46a2e08cd703103cc1e14f9c1610e75b06d52502
OmarMWarraich <earthling986@gmail.com>
2023-02-12 21:02:35 +0500

    feat: ReDeploy Contract incl Logging

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,13 +1,18 @@
-import { storage, Context } from "near-sdk-as";
+import { storage, Context, logging } from "near-sdk-as";
 
 export class Greeting {
 
     getGreeting(accountId: string):string | null {
+        
+        logging.log("Getting greeting for " + accountId);
 
         return storage.get<string>(accountId, "Greeting not available");
     }
 
     setGreeting(greeting: string): void{
+
+        logging.log("Setting greeting for " + Context.sender);
+        logging.log("Greeting is " + greeting);
         storage.set<string>(Context.sender, greeting);
     }
 }
